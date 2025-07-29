0a6820f6eb48a5ff29a63209cae02c1a7c28a390
sshahab2008 <shamim_99@yahoo.com>
2025-07-28 22:51:24 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI for us-east-1 (update as needed)
+  ami           = "ami-05ffe3c48a9991133" # Amazon Linux 2 AMI for us-east-1 (update as needed)
   instance_type = "t2.micro"
 
   tags = {
