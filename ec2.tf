544175984198a25e9c8a6026e1c0e30cbb75397c
sshahab2008 <shamim_99@yahoo.com>
2025-08-05 10:27:02 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,4 @@ resource "aws_instance" "example" {
   ami           = "ami-05ffe3c48a9991133" # Amazon Linux 2 AMI for us-east-1 (update as needed)
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "ExampleInstance"
-  }
 }
